127d81e18bff0f5ec74174b367e9c76cc3c6317b
vachagunn <vachikpuper@gmail.com>
2024-02-19 13:50:31 +0500

    новая страница + анимация

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
 const yesBtn = document.querySelector('.yes');
 const noBtn = document.querySelector('.no');
 
-const doYes = () => {}
+const doYes = () => location.href = 'love_page.html';
 
-const doNo = () =>  noBtn.remove();
+const doNo = () =>  {
+    noBtn.remove();
+    yesBtn.style.fontSize = 2 + 'em';
+    yesBtn.textContent = 'Of course yes!!!';
+};
 
 yesBtn.addEventListener('click', doYes);
 noBtn.addEventListener('click', doNo);
